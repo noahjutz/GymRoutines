41a2eccf86f3e56cd2a45dce33cd9b9c10881612
Noah Jutz <noahjutz@tutanota.de>
2021-11-19 18:49:52 +0100

    Add Roboto Slab font

---
+++ app/src/main/java/com/noahjutz/gymroutines/ui/theme/Type.kt
@@ -0,0 +1,10 @@
+package com.noahjutz.gymroutines.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.noahjutz.gymroutines.R
+
+val RobotoSlab = FontFamily(
+    Font(R.font.roboto_slab_black, FontWeight.W900)
+)
