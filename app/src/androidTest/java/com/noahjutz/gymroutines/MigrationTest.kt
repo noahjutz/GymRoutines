61c2a78b5887d04f244c2da706af03a9fa750099
Noah Jutz <noahjutz@tutanota.de>
2021-11-19 13:51:17 +0100

    Add migrateAll test

--- app/src/androidTest/java/com/noahjutz/gymroutines/MigrationTest.kt
+++ app/src/androidTest/java/com/noahjutz/gymroutines/MigrationTest.kt
@@ -1,5 +1,6 @@
 package com.noahjutz.gymroutines
 
+import androidx.room.Room
 import androidx.room.testing.MigrationTestHelper
 import androidx.sqlite.db.framework.FrameworkSQLiteOpenHelperFactory
 import androidx.test.platform.app.InstrumentationRegistry
@@ -11,10 +12,10 @@ import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import java.io.IOException
 
+private const val TEST_DB = "migration-test"
+
 @RunWith(JUnit4::class)
 class MigrationTest {
-    private val TEST_DB = "migration-test"
-
     @get:Rule
     val helper: MigrationTestHelper = MigrationTestHelper(
         InstrumentationRegistry.getInstrumentation(),
@@ -22,6 +23,30 @@ class MigrationTest {
         emptyList(),
         FrameworkSQLiteOpenHelperFactory()
     )
+
+    @Test
+    @Throws(IOException::class)
+    fun migrateAll() {
+        // Create earliest version of the database.
+        helper.createDatabase(TEST_DB, 35).apply {
+            close()
+        }
+
+        // Open latest version of the database. Room will validate the schema
+        // once all migrations execute.
+        Room
+            .databaseBuilder(
+                InstrumentationRegistry.getInstrumentation().targetContext,
+                AppDatabase::class.java,
+                TEST_DB
+            )
+            .addMigrations(MIGRATION_36_37)
+            .build()
+            .apply {
+                openHelper.writableDatabase
+                close()
+            }
+    }
 
     @Test
     @Throws(IOException::class)
