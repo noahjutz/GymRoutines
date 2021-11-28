8382de047c8ac7c958c24192df0f575f72640494
Noah Jutz <noahjutz@tutanota.de>
2021-11-28 15:09:57 +0100

    Remove interactivity from WorkoutViewer set groups

--- app/src/main/java/com/noahjutz/gymroutines/ui/workout/viewer/WorkoutViewer.kt
+++ app/src/main/java/com/noahjutz/gymroutines/ui/workout/viewer/WorkoutViewer.kt
@@ -1,34 +1,33 @@
 package com.noahjutz.gymroutines.ui.workout.viewer
 
-import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.noahjutz.gymroutines.data.domain.WorkoutWithSetGroups
 import com.noahjutz.gymroutines.data.domain.duration
-import com.noahjutz.gymroutines.ui.components.AutoSelectTextField
 import com.noahjutz.gymroutines.ui.components.TopBar
-import com.noahjutz.gymroutines.ui.components.durationVisualTransformation
-import com.noahjutz.gymroutines.util.RegexPatterns
 import com.noahjutz.gymroutines.util.formatSimple
 import com.noahjutz.gymroutines.util.pretty
 import com.noahjutz.gymroutines.util.toStringOrBlank
@@ -104,48 +103,9 @@ fun WorkoutViewerContent(workout: WorkoutWithSetGroups, viewModel: WorkoutViewer
                                 exercise?.name.toString(),
                                 style = typography.h5,
                                 modifier = Modifier
-                                    .padding(16.dp)
+                                    .padding(24.dp)
                                     .weight(1f)
                             )
-
-                            Box {
-                                var expanded by remember { mutableStateOf(false) }
-                                IconButton(
-                                    modifier = Modifier.padding(16.dp),
-                                    onClick = { expanded = !expanded }
-                                ) {
-                                    Icon(Icons.Default.DragHandle, "More")
-                                }
-                                DropdownMenu(
-                                    expanded = expanded,
-                                    onDismissRequest = { expanded = false }
-                                ) {
-                                    DropdownMenuItem(onClick = {
-                                        expanded = false
-                                        val id = setGroup.group.id
-                                        val toId = workout.setGroups
-                                            .find { it.group.position == setGroup.group.position - 1 }
-                                            ?.group
-                                            ?.id
-                                        if (toId != null) {
-                                        }
-                                    }) {
-                                        Text("Move Up")
-                                    }
-                                    DropdownMenuItem(onClick = {
-                                        expanded = false
-                                        val id = setGroup.group.id
-                                        val toId = workout.setGroups
-                                            .find { it.group.position == setGroup.group.position + 1 }
-                                            ?.group
-                                            ?.id
-                                        if (toId != null) {
-                                        }
-                                    }) {
-                                        Text("Move Down")
-                                    }
-                                }
-                            }
                         }
                     }
                     Column(Modifier.padding(vertical = 16.dp)) {
@@ -257,155 +217,69 @@ fun WorkoutViewerContent(workout: WorkoutWithSetGroups, viewModel: WorkoutViewer
                                     Row(
                                         Modifier.padding(horizontal = 4.dp)
                                     ) {
-                                        val textFieldStyle = typography.body1.copy(
-                                            textAlign = TextAlign.Center,
-                                            color = MaterialTheme.colors.onSurface
-                                        )
-                                        val decorationBox: @Composable (@Composable () -> Unit) -> Unit =
-                                            { innerTextField ->
-                                                Surface(
-                                                    color = MaterialTheme.colors.onSurface.copy(alpha = 0.1f),
-                                                    shape = RoundedCornerShape(8.dp),
+                                        val TableCell: @Composable RowScope.(@Composable BoxScope.() -> Unit) -> Unit =
+                                            {
+                                                ProvideTextStyle(
+                                                    value = typography.body1.copy(
+                                                        textAlign = TextAlign.Center,
+                                                        color = MaterialTheme.colors.onSurface
+                                                    )
                                                 ) {
-                                                    Box(
-                                                        Modifier
-                                                            .padding(horizontal = 4.dp)
-                                                            .height(56.dp),
-                                                        contentAlignment = Alignment.Center
+                                                    Surface(
+                                                        modifier = Modifier
+                                                            .padding(4.dp)
+                                                            .weight(1f),
+                                                        color = MaterialTheme.colors.onSurface.copy(
+                                                            alpha = 0.1f
+                                                        ),
+                                                        shape = RoundedCornerShape(8.dp),
                                                     ) {
-                                                        innerTextField()
+                                                        Box(
+                                                            Modifier
+                                                                .padding(horizontal = 4.dp)
+                                                                .height(56.dp),
+                                                            contentAlignment = Alignment.Center,
+                                                            content = it
+                                                        )
                                                     }
                                                 }
 
                                             }
                                         if (exercise?.logReps == true) {
-                                            val (reps, setReps) = remember { mutableStateOf(set.reps.toStringOrBlank()) }
-                                            LaunchedEffect(reps) {
-                                                val repsInt = reps.toIntOrNull()
-                                            }
-                                            AutoSelectTextField(
-                                                modifier = Modifier
-                                                    .weight(1f)
-                                                    .padding(4.dp),
-                                                value = reps,
-                                                onValueChange = {
-                                                    if (it.matches(RegexPatterns.integer))
-                                                        setReps(it)
-                                                },
-                                                textStyle = textFieldStyle,
-                                                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                                                singleLine = true,
-                                                cursorColor = MaterialTheme.colors.onSurface,
-                                                decorationBox = decorationBox,
-                                            )
+                                            TableCell { Text(set.reps.toStringOrBlank()) }
                                         }
                                         if (exercise?.logWeight == true) {
-                                            val (weight, setWeight) = remember { mutableStateOf(set.weight.formatSimple()) }
-                                            LaunchedEffect(weight) {
-                                                val weightDouble = weight.toDoubleOrNull()
-                                            }
-                                            AutoSelectTextField(
-                                                modifier = Modifier
-                                                    .weight(1f)
-                                                    .padding(4.dp),
-                                                value = weight,
-                                                onValueChange = {
-                                                    if (it.matches(RegexPatterns.float))
-                                                        setWeight(it)
-                                                },
-                                                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                                                singleLine = true,
-                                                textStyle = textFieldStyle,
-                                                cursorColor = MaterialTheme.colors.onSurface,
-                                                decorationBox = decorationBox
-                                            )
+                                            TableCell { Text(set.weight.formatSimple()) }
                                         }
                                         if (exercise?.logTime == true) {
-                                            val (time, setTime) = remember { mutableStateOf(set.time.toStringOrBlank()) }
-                                            LaunchedEffect(time) {
-                                                val timeInt = time.toIntOrNull()
-                                            }
-                                            AutoSelectTextField(
-                                                modifier = Modifier
-                                                    .weight(1f)
-                                                    .padding(4.dp),
-                                                value = time,
-                                                onValueChange = {
-                                                    if (it.matches(RegexPatterns.duration))
-                                                        setTime(it)
-                                                },
-                                                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                                                singleLine = true,
-                                                textStyle = textFieldStyle,
-                                                visualTransformation = durationVisualTransformation,
-                                                cursorColor = MaterialTheme.colors.onSurface,
-                                                decorationBox = decorationBox
-                                            )
+                                            TableCell { Text(set.time.toStringOrBlank()) }
                                         }
                                         if (exercise?.logDistance == true) {
-                                            val (distance, setDistance) = remember {
-                                                mutableStateOf(
-                                                    set.distance.formatSimple()
-                                                )
-                                            }
-                                            LaunchedEffect(distance) {
-                                                val distanceDouble = distance.toDoubleOrNull()
-                                            }
-                                            AutoSelectTextField(
-                                                modifier = Modifier
-                                                    .weight(1f)
-                                                    .padding(4.dp),
-                                                value = distance,
-                                                onValueChange = {
-                                                    if (it.matches(RegexPatterns.float))
-                                                        setDistance(it)
-                                                },
-                                                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                                                singleLine = true,
-                                                textStyle = textFieldStyle,
-                                                cursorColor = MaterialTheme.colors.onSurface,
-                                                decorationBox = decorationBox
-                                            )
+                                            TableCell { Text(set.distance.formatSimple()) }
                                         }
                                         Box(
                                             Modifier
                                                 .padding(4.dp)
                                                 .size(56.dp)
                                                 .clip(RoundedCornerShape(8.dp))
-                                                .toggleable(
-                                                    value = set.complete,
-                                                    onValueChange = {
-                                                    },
-                                                )
                                                 .background(
-                                                    animateColorAsState(
-                                                        if (set.complete) MaterialTheme.colors.secondary else MaterialTheme.colors.onSurface.copy(
-                                                            alpha = 0.1f
-                                                        )
-                                                    ).value
+                                                    if (set.complete) MaterialTheme.colors.secondary
+                                                    else MaterialTheme.colors.onSurface.copy(
+                                                        alpha = 0.1f
+                                                    )
                                                 ),
                                             contentAlignment = Alignment.Center
                                         ) {
                                             Icon(
                                                 Icons.Default.Check,
                                                 "Complete",
-                                                tint = animateColorAsState(if (set.complete) MaterialTheme.colors.onSecondary else MaterialTheme.colors.onSurface).value
+                                                tint = if (set.complete) MaterialTheme.colors.onSecondary else MaterialTheme.colors.onSurface
                                             )
                                         }
                                     }
                                 }
                             }
                         }
-                    }
-                    TextButton(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(64.dp),
-                        onClick = {  },
-                    ) {
-                        Icon(Icons.Default.Add, null)
-                        Spacer(Modifier.width(12.dp))
-                        Text("Add Set")
                     }
                 }
             }
