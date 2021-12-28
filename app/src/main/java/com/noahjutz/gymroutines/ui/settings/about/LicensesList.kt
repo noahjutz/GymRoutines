49ac7efea7584da580648a593d806eaa2d7c99ae
Noah Jutz <noahjutz@tutanota.de>
2021-12-28 16:01:23 +0100

    Update LicensesList

--- app/src/main/java/com/noahjutz/gymroutines/ui/settings/about/LicensesList.kt
+++ app/src/main/java/com/noahjutz/gymroutines/ui/settings/about/LicensesList.kt
@@ -2,18 +2,23 @@ package com.noahjutz.gymroutines.ui.settings.about
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.OpenInBrowser
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.noahjutz.gymroutines.BuildConfig
 import com.noahjutz.gymroutines.R
 import com.noahjutz.gymroutines.ui.components.TopBar
 import com.noahjutz.gymroutines.util.openUrl
@@ -30,22 +35,51 @@ data class Dependency(
 )
 
 val dependencies = listOf(
-    Dependency("Koin", Licenses.APACHE2, "https://github.com/InsertKoinIO/koin"),
-    Dependency("Android Jetpack", Licenses.APACHE2, "https://developer.android.com/jetpack"),
     Dependency(
-        "kotlinx.coroutines",
-        Licenses.APACHE2,
-        "https://github.com/Kotlin/kotlinx.coroutines"
+        name = "Koin",
+        license = Licenses.APACHE2,
+        url = "https://github.com/InsertKoinIO/koin"
     ),
     Dependency(
-        "kotlinx.serialization",
-        Licenses.APACHE2,
-        "https://github.com/Kotlin/kotlinx.serialization"
+        name = "Android Jetpack",
+        license = Licenses.APACHE2,
+        url = "https://developer.android.com/jetpack"
     ),
-    Dependency("ProcessPhoenix", Licenses.APACHE2, "https://github.com/JakeWharton/ProcessPhoenix"),
-    Dependency("Junit 4", Licenses.EPL1, "https://github.com/junit-team/junit4"),
-    Dependency("AssertJ", Licenses.APACHE2, "https://github.com/assertj/assertj-core"),
-    Dependency("MockK", Licenses.APACHE2, "https://github.com/mockk/mockk")
+    Dependency(
+        name = "Kotlinx Coroutines",
+        license = Licenses.APACHE2,
+        url = "https://github.com/Kotlin/kotlinx.coroutines"
+    ),
+    Dependency(
+        name = "Kotlinx Serialization",
+        license = Licenses.APACHE2,
+        url = "https://github.com/Kotlin/kotlinx.serialization"
+    ),
+    Dependency(
+        name = "ProcessPhoenix",
+        license = Licenses.APACHE2,
+        url = "https://github.com/JakeWharton/ProcessPhoenix"
+    ),
+    Dependency(
+        name = "Junit 4",
+        license = Licenses.EPL1,
+        url = "https://github.com/junit-team/junit4"
+    ),
+    Dependency(
+        name = "AssertJ",
+        license = Licenses.APACHE2,
+        url = "https://github.com/assertj/assertj-core"
+    ),
+    Dependency(
+        name = "MockK",
+        license = Licenses.APACHE2,
+        url = "https://github.com/mockk/mockk"
+    ),
+    Dependency(
+        name = "Accompanist",
+        license = Licenses.APACHE2,
+        url = "https://github.com/google/accompanist"
+    )
 )
 
 @ExperimentalMaterialApi
@@ -53,7 +87,6 @@ val dependencies = listOf(
 fun LicensesList(
     popBackStack: () -> Unit,
 ) {
-    val context = LocalContext.current
     Scaffold(
         topBar = {
             TopBar(
@@ -66,23 +99,29 @@ fun LicensesList(
     ) {
         LazyColumn {
             item {
-                Card(Modifier.padding(16.dp)) {
-                    Box(Modifier.padding(8.dp)) {
+                Card(
+                    Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp),
+                    shape = RoundedCornerShape(30.dp)
+                ) {
+                    Box(Modifier.padding(20.dp)) {
                         Text(stringResource(R.string.license_notice), style = typography.body2)
                     }
                 }
                 Text(
                     "Open-Source Dependencies",
-                    style = typography.h5,
-                    modifier = Modifier.padding(horizontal = 16.dp)
+                    style = typography.h4,
+                    modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp)
                 )
+                Spacer(Modifier.height(16.dp))
             }
 
             items(dependencies) { dependency ->
+                val context = LocalContext.current
                 ListItem(
                     modifier = Modifier.clickable { context.openUrl(dependency.url) },
                     text = { Text(dependency.name) },
-                    secondaryText = { Text(dependency.license.fullName) }
+                    secondaryText = { Text(dependency.license.fullName) },
+                    trailing = { Icon(Icons.Default.OpenInBrowser, "Open in browser") }
                 )
             }
         }
