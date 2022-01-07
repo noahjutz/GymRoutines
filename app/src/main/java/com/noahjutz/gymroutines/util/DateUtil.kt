d0fbbc82d1a756c88697215089f0bdeb0e6f26d9
Noah Jutz <noahjutz@tutanota.de>
2022-01-07 23:01:35 +0100

    Add Duration.iso8601()
    currently unused

--- app/src/main/java/com/noahjutz/gymroutines/util/DateUtil.kt
+++ app/src/main/java/com/noahjutz/gymroutines/util/DateUtil.kt
@@ -1,13 +1,22 @@
 package com.noahjutz.gymroutines.util
 
 import java.text.DateFormat
-import java.util.Date
+import java.util.*
 import kotlin.time.Duration
-import kotlin.time.ExperimentalTime
 
 fun Date.formatSimple(): String {
     return DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT).format(this)
 }
 
-@OptIn(ExperimentalTime::class)
-fun Duration.pretty(): String = this.toComponents { h, m, _, _ -> "${h}h ${m}min" }
+fun Duration.pretty(): String = toComponents { h, m, _, _ -> "${h}h ${m}min" }
+
+fun Duration.iso8601(): String = toComponents { h, m, s, _ ->
+    (if (h.toString().length < 2) "0" else "") +
+            h.toString() +
+            ":" +
+            (if (m.toString().length < 2) "0" else "") +
+            m.toString() +
+            ":" +
+            (if (s.toString().length < 2) "0" else "") +
+            s.toString()
+}
