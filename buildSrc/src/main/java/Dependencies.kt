da68fafe9efd97994ebed973416500c7e32ab703
Noah Jutz <noah@noahjutz.com>
2023-02-09 14:35:15 +0100

    Upgrade material dependency

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -38,7 +38,7 @@ object Libs {
     }
 
     object Material {
-        private const val version = "1.6.1"
+        private const val version = "1.8.0"
         const val material = "com.google.android.material:material:$version"
     }
 
