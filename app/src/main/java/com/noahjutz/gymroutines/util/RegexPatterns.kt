77fe80cb3d67e57cd1d0bf3df42cfdf7fcb58408
spencerwi <spencerwi@gmail.com>
2023-02-24 17:08:06 -0500

    Allow weights for a workout set to be higher than 999lbs
    Believe it or not, I have a use-case for this! There's one particular exercise I do at my local gym -- the leg press -- where I do 1000lbs (sometimes slightly more) per rep.
    
    I can sorta approximate this by just going with 999 (the current maximum allowable weight), but...it's an approximation.
    
    This PR allows 4-digit numbers as weights for a workout set as long as that number begins with 1.

--- app/src/main/java/com/noahjutz/gymroutines/util/RegexPatterns.kt
+++ app/src/main/java/com/noahjutz/gymroutines/util/RegexPatterns.kt
@@ -22,7 +22,7 @@ object RegexPatterns {
     val integer =
         """^$|^0$|^[1-9]\d{0,2}$""".toRegex()
     val float =
-        """^(0|[1-9]\d{0,2})?((?<=\d)\.)?((?<=\.)\d{1,3}$)?""".toRegex()
+        """^(0|1?[1-9]\d{0,2})?((?<=\d)\.)?((?<=\.)\d{1,3}$)?""".toRegex()
     val duration =
         """^$|[1-9]\d{0,3}""".toRegex()
 }
