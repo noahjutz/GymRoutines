f3c35f10ba4573ffc42105cd8c5f3f62bd03d954
Noah Jutz <noahjutz@tutanota.de>
2023-08-03 16:22:16 +0200

    Upgrade AGP to v8.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,13 +49,13 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
-    }
+    //kotlinOptions {
+    //    jvmTarget = JavaVersion.VERSION_1_8.toString()
+    //}
 
     lint {
         textReport = true
@@ -98,6 +98,7 @@ android {
             )
         )
     }
+    namespace = "com.noahjutz.gymroutines"
 }
 
 dependencies {
--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.4.1")
+        classpath("com.android.tools.build:gradle:8.1.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
     }
 }
